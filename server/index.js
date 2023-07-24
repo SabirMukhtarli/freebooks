a4484626ffde3943f906657503a5a3f2bb557a0a
Sabir <sabirmuxtarli@gmail.com>
2023-07-24 21:23:05 +0400

    First commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+});
